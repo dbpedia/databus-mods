136b0f4a1c09cd738bd48236e98728e596b76772
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-08-22 18:37:52 +0200

    fix

--- online-check-mod/src/main/scala/check_if_online.scala
+++ online-check-mod/src/main/scala/check_if_online.scala
@@ -86,7 +86,24 @@ object check_if_online {
   def writeHTMLSummary ( htmlFile:String, statFile : String) ={
     val header =
       s"""
+         |<!DOCTYPE html>
          |<html>
+         |<head>
+         |<style>
+         |table {
+         |  border-collapse: collapse;
+         |  width: 100%;
+         |}
+         |
+         |th, td {
+         |  text-align: left;
+         |  padding: 8px;
+         |  border: 1px solid black;
+         |}
+         |
+         |tr:nth-child(even) {background-color: #f2f2f2;}
+         |</style>
+         |</head>
          |<body>
          |<table style="width:100%">
          |  <tr>
