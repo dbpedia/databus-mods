840759936429cdfb26b57a6cb08b7429d3fe2e27
kurzum <kurzum@github.com>
2020-03-31 12:37:18 +0200

    sparql todo

--- indexer/src/main/scala/org/dbpedia/databus/indexer/Index.scala
+++ indexer/src/main/scala/org/dbpedia/databus/indexer/Index.scala
@@ -7,7 +7,7 @@ object  Index {
 
 
   def main(args: Array[String]): Unit = {
-    updateIndex("")
+    updateIndex("dbpedia/mappings/%")
   }
 
   /**
@@ -17,28 +17,25 @@ object  Index {
   def updateIndex(pattern:String){
 
 
-    val sparql = """PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>
-                   |PREFIX dct:    <http://purl.org/dc/terms/>
-                   |PREFIX dcat:   <http://www.w3.org/ns/dcat#>
-                   |PREFIX db:     <https://databus.dbpedia.org/>
-                   |PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                   |PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>
-                   |
-                   |SELECT * WHERE
-                   |{   	?dataset dataid:version ?version .
-                   |        FILTER (?version LIKE <https://databus.dbpedia.org/dbpedia/%> ).
-                   |}
-                   |""".stripMargin
+    val sparql = s"""
+       |PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>
+       |PREFIX dct:    <http://purl.org/dc/terms/>
+       |PREFIX dcat:   <http://www.w3.org/ns/dcat#>
+       |PREFIX db:     <https://databus.dbpedia.org/>
+       |PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+       |PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>
+       |
+       |SELECT ?dataset ?version ?distribution ?downloadURL ?shaSum WHERE
+       |{
+       |  ?dataset dataid:version ?version .
+       |  FILTER (?version LIKE <https://databus.dbpedia.org/${pattern}> ).
+       |  ?dataset dcat:distribution ?distribution .
+       |  ?distribution  dcat:downloadURL ?downloadURL .
+       |  ?distribution  dataid:sha256sum ?shaSum .
+       |}
+       |""".stripMargin
 
-      //TODO sparql and save
-      val p ="dbpedia/generic/*/*"
-      val s: Array[String] = p.split('/')
-      val account = s.apply(0)
-      val group = s.apply(1)
-      val artifact = s.apply(2)
-      val version = s.apply(3)
-
-      if (version!= "*"
+        //TODO SPARQL all with limit and offset
 
 
 
