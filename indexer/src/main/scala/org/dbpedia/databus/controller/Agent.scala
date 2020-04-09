5816ceeabbffecaad16715fc7004e3810ba49c27
kurzum <kurzum@github.com>
2020-04-09 17:31:24 +0200

    todos fabian

--- indexer/src/main/scala/org/dbpedia/databus/controller/Agent.scala
+++ indexer/src/main/scala/org/dbpedia/databus/controller/Agent.scala
@@ -10,8 +10,13 @@ class Agent ( val processors: java.util.List[Processor], val sink:Sink ) {
   def process(item:Item) = {
 
     // download / check if downloaded
+    //TODO Fabian databus client
+    //add a dir variable and whatever you need.
+    //Note that you can also change the variables in Item or tell me to change them in case you need different fields
 
     // process and sink
+    // TODO Fabian
+    // Processor.scala needs to extended, as def process method needs more variables, such as the filename, etc. 
     processors.forEach(_.process(item,sink))
 
 
---
+++ indexer/src/main/scala/org/dbpedia/databus/process/MimeTypeProcessor.scala
@@ -0,0 +1,11 @@
+package org.dbpedia.databus.process
+import org.dbpedia.databus.indexer.Item
+import org.dbpedia.databus.sink.Sink
+
+
+class MimeTypeProcessor extends Processor {
+  override def process(item: Item, sink: Sink): Unit = {
+    //TODO FABIAN
+
+  }
+}
